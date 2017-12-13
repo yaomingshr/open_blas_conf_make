5f69102f9d55ddcea3a4f05ac1f96eac1335da1d
patrickyao <patrickyao@tencent.com>
2017-12-13 17:47:38 +0800

    first commit

---
+++ OpenBLAS-0.2.20/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.20")
+SET(OpenBLAS_INCLUDE_DIRS /data4/kimmyzhang/OpenBLAS-0.2.20/include)
+SET(OpenBLAS_LIBRARIES /data4/kimmyzhang/OpenBLAS-0.2.20/lib/libopenblas.so)
---
+++ README.md
@@ -0,0 +1,1 @@
+# open_blas_conf_make
